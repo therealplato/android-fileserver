fb6303ccd8147726e772e9e1d1bcf4361e347819
plato-cambrian <plato@cambrian.org>
2015-04-07 02:03:52 -0600

    Receive the message intent

---
+++ app/src/main/java/com/therealplato/fileserver/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package com.therealplato.fileserver;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(FileserverActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+
+
+}
--- app/src/main/java/com/therealplato/fileserver/FileserverActivity.java
+++ app/src/main/java/com/therealplato/fileserver/FileserverActivity.java
@@ -1,19 +1,29 @@
 package com.therealplato.fileserver;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class FileserverActivity extends ActionBarActivity {
-
+    public final static String EXTRA_MESSAGE = "com.therealplato.fileserver.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fileserver);
     }
 
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
